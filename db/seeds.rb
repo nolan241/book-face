ff9d2f9d1be987cfb0316677ecdcb674332a1032
Nolan Morache <nolan241@gmail.com>
2016-09-20 02:43:45 +0000

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "nolanm87", email: "nolanm87@gmail.com", password: "thrasher241", password_confirmation: "thrasher241")
+User.create(username: "nolanm88", email: "nolanm88@gmail.com", password: "thrasher241", password_confirmation: "thrasher241")
+User.create(username: "nolanm89", email: "nolanm89@gmail.com", password: "thrasher241", password_confirmation: "thrasher241")
+User.create(username: "nolanm90", email: "nolanm90@gmail.com", password: "thrasher241", password_confirmation: "thrasher241")
+User.create(username: "nolanm91", email: "nolanm91@gmail.com", password: "thrasher241", password_confirmation: "thrasher241")
+p "Test users created"
