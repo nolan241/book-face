8a6f75f024b6181096f76d2158131c8d6c5f828e
Nolan Morache <nolan241@gmail.com>
2016-09-29 20:52:14 +0000

    Create friend filter on user to display friend status on the user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
+  
+  before_action :authenticate_user!, only:[:index]
+    #require user to be signed in to view index
+  
   before_action :set_user, only:[:show]
   
   def index
-    @users = User.all
+    case params[:people] 
+      when "friends"
+        #If the url is ->  localhost:3000/users?friends  it will display your friends.
+        @users = current_user.active_friends
+      when "requests"
+        #If the url is ->  localhost:3000/users?requests  it will display your requests.
+        @users = current_user.pending_friend_requests_from.map(&:friend)
+      when "pending"
+        #If the url is ->  localhost:3000/users?pending  it will display the request that are pending for your approval.
+        @users = current_user.pending_friend_requests_to.map(&:friend)
+      else
+        #all users except for the current user
+        @users = User.where.not(id: current_user.id)
+    end
   end
   
   def show
